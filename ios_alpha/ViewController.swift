bc374feb42c59aa91925342bfbc312532a694cd9
Bruno LE HYARIC <bu2github@gmail.com>
2020-07-26 16:54:40 +0200

    Initial Commit

---
+++ ios_alpha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_alpha
+//
+//  Created by bu2 on 26/07/2020.
+//  Copyright © 2020 bu2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
