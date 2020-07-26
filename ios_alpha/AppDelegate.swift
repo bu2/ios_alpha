02c9210aec5b5de248d71ae959a05ad755b67266
Bruno LE HYARIC <bu2github@gmail.com>
2020-07-26 18:03:27 +0200

    Initial commit.

--- ios_alpha/AppDelegate.swift
+++ ios_alpha/AppDelegate.swift
@@ -7,31 +7,39 @@
 //
 
 import UIKit
+import Turbolinks
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    var window: UIWindow?
+    var navigationController = UINavigationController()
+    var session = Session()
+    
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        window?.rootViewController = navigationController
+        startApplication()
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    func startApplication() {
+        session.delegate = self
+        visit(URL: URL(string: "http://bu2book.local:3000/programs")!)
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    func visit(URL: URL) {
+        let visitableViewController = VisitableViewController(url: URL)
+        navigationController.pushViewController(visitableViewController, animated: true)
+        session.visit(visitableViewController)
     }
-
-
 }
 
+extension AppDelegate: SessionDelegate {
+    func session(_ session: Session, didProposeVisitToURL URL: URL, withAction action: Action) {
+        visit(URL: URL)
+    }
+    
+    func session(_ session: Session, didFailRequestForVisitable visitable: Visitable, withError error: NSError) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        navigationController.present(alert, animated: true, completion: nil)
+    }
+}
